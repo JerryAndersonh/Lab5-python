10f5d44b7670c456289a8be3816c270445017906
JerryAndersonh <jhuaynacho@unsa.edu.pe>
2025-05-12 23:40:31 -0500

    Ejercicio 2d una fila del tablero

--- Ejercicio2d.py
+++ Ejercicio2d.py
@@ -1,2 +1,11 @@
+from chessPictures import SQUARE
 from interpreter import draw
-from chessPictures import *
+from picture import Picture
+
+blanco = Picture(SQUARE)
+negro = blanco.negative()
+
+# Alternamos blanco y negro
+fila = blanco.join(negro).horizontalRepeat(4)
+
+draw(fila)
