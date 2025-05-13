7ea7ae579e5c94db240a1bdd7c13641069637b59
Jerry Anderson Huaynacho Mango <168607325+JerryAndersonh@users.noreply.github.com>
2025-05-13 20:01:35 +0000

    Casilleros negro|blanco|negro..

--- Ejercicio2e.py
+++ Ejercicio2e.py
@@ -1,2 +1,11 @@
+from chessPictures import SQUARE
 from interpreter import draw
-from chessPictures import *
+from picture import Picture
+
+blanco = Picture(SQUARE)
+negro = blanco.negative()
+
+# Alternamos negro y blanco, empezando con negro
+fila = negro.join(blanco).horizontalRepeat(4)
+
+draw(fila)
